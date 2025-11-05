e805b79de321dac5eeffd9767cfadab639caeb3f
sikdong <zmstnv1119@naver.com>
2025-11-05 19:44:31 +0900

    init

---
+++ backend/src/main/java/com/gomdol/vintageshop/VintageShopApplication.java
@@ -0,0 +1,12 @@
+package com.gomdol.vintageshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VintageShopApplication.class, args);
+    }
+}
